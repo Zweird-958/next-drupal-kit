702a9ea0da348197e1154d9aebf13d6594f0c230
HAMEL Julien <zweir.dd@gmail.com>
2024-05-03 14:09:46 +0200

    initial commit

---
+++ components/layout.jsx
@@ -0,0 +1,37 @@
+import { Footer, Header, PreviewRibbon } from '@pantheon-systems/nextjs-kit';
+import SearchInput from './search-input';
+import styles from './layout.module.css';
+
+export default function Layout({ children, footerMenu, preview = false }) {
+	const navItems = [
+		{ linkText: '🏠 Home', href: '/' },
+		{ linkText: '📰 Articles', href: '/articles' },
+		{ linkText: '📑 Pages', href: '/pages' },
+		{ linkText: '⚛️ Examples', href: '/examples' },
+	];
+	const footerMenuItems = footerMenu?.map(({ title, url, parent }) => ({
+		linkText: title,
+		href: url,
+		parent: parent,
+	}));
+
+	return (
+		<div className={`${styles.layout} flex flex-col`}>
+			{preview && <PreviewRibbon />}
+			<div
+				className={`${styles.searchHeaderContainer} flex flex-row flex-wrap justify-center`}
+			>
+				<Header navItems={navItems} />
+				<SearchInput />
+			</div>
+			<main className="mb-auto">{children}</main>
+			<Footer footerMenuItems={footerMenuItems}>
+				<span className="my-0 mx-auto">
+					© {new Date().getFullYear()} Built with{' '}
+					<a href="https://nextjs.org/">Next.js</a> and{' '}
+					<a href="https://www.drupal.org/">Drupal</a>
+				</span>
+			</Footer>
+		</div>
+	);
+}
